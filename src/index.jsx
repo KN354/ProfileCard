00c2865d7ddc29f0b0166cd89543404b9ca42c75
KN354 <karthikn354@gmail.com>
2026-09-17 00:04:25 +0530

    Profile card update

--- src/index.jsx
+++ src/index.jsx
@@ -31,6 +31,7 @@ function SkillSet() {
       <Skill Skill="WPF" Color="Red" Emoji="💪" />
       <Skill Skill="CAD Programming" Color="Orange" Emoji="💪" />
       <Skill Skill="Digital Twin" Color="Orange" Emoji="💪" />
+      <Skill Skill="React JS" Color="Orange" Emoji="💪" />
     </div>
   );
 }
